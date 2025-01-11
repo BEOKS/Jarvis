ce3d7d341b73fa27c151a20fe2ec0d03512be86e
BEOKS <lee01042000@gmail.com>
2025-01-11 15:18:46 +0900

    initial commit

---
+++ src/test/kotlin/com/beoks/jarvis/JarvisApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.beoks.jarvis
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JarvisApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
