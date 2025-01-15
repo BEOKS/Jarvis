c6384e2e5d30a06d63348b6d50a244e42fc9cfd9
Zayden(이재성) <leejs@gabia.com>
2025-01-15 16:29:43 +0900

    append slf4j provider

--- jarvis-core/build.gradle.kts
+++ jarvis-core/build.gradle.kts
@@ -10,7 +10,7 @@ repositories {
     mavenCentral()
 }
 
-val ktor_version: String ="3.0.3"
+val ktor_version: String = "3.0.3"
 
 dependencies {
     // https://mvnrepository.com/artifact/com.squareup.okhttp3/okhttp
@@ -19,6 +19,8 @@ dependencies {
     implementation("io.ktor:ktor-client-cio:$ktor_version")
     implementation("io.ktor:ktor-client-content-negotiation:$ktor_version")
     implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
+    implementation("org.slf4j:slf4j-api:2.0.5")
+    runtimeOnly("org.slf4j:slf4j-simple:2.0.5")
     testImplementation(kotlin("test"))
 }
 
